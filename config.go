5280ffd352106699ec3b576c0dba6023b66b7799
pa001024 <pa001024@qq.com>
2013-06-27 09:38:54 +0800

    Tweak config

--- config.go
+++ config.go
@@ -1,18 +1,44 @@
 package main
 
-import ()
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
 
 type JobConfig struct {
-	Name   string      `json:"name"`
-	Source []JobSource `json:"source"`
-	Target []JobTarget `json:"target"`
+	Source []*JobSource `json:"source"`
+	Target []*JobTarget `json:"target"`
 }
 
 type JobSource struct {
-	Name string `json:"name"`
-	Url  string `json:"url"`
+	Type      string `json:"type"`
+	Format    string `json:"format"`
+	Name      string `json:"name"`
+	Url       string `json:"url"`
+	EnablePic string `json:"enablepic"`
+	PicTag    string `json:"pictag"`
+	PicSize   []int  `json:"picsize"`
 }
 
 type JobTarget struct {
-	Name string `json:"name"`
+	Type   string             `json:"type"`
+	Name   string             `json:"name"`
+	Method []*JobTargetMethod `json:"method"`
+	// 微博部分
+	AppKey      string    `json:client_id`     // AppKey
+	AppSecret   string    `json:client_secret` // AppSecret
+	CallbackUrl string    `json:redirect_uri`  // 验证URL
+	Token       string    `json:access_token`  // OAuth2.0 验证码
+	ExpiresIn   time.Time `json:expires_in`    // 失效时间
+}
+
+type JobTargetMethod struct {
+	Action string   `json:"action"`
+	Source []string `json:"source"`
+}
+
+func (this *JobConfig) Load(r io.Reader) (err error) {
+	json.NewDecoder(r).Decode(this)
+	return
 }

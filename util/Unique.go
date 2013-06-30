1b39783f28752194abc978be2bc016b59869c647
pa001024 <pa001024@qq.com>
2013-06-30 10:01:37 +0800

    fix Unique

--- util/Unique.go
+++ util/Unique.go
@@ -2,46 +2,46 @@ package util
 
 import (
 	"encoding/json"
-	"io"
 )
 
-type Unique interface { // 禁止重复
-	Put(id string) bool    // 检测唯一并设置
-	CanPut(id string) bool // 检测唯一
+type Unique interface { // 禁止重复更新
+	Put(id string) (err error) // 检测唯一并设置
+	CanPut(id string) bool     // 检测唯一
 }
 
-func NewUnrepeatable(typ string, args map[string]string) (this Unique) { // 工厂函数
+func NewUnique(typ string, args map[string]string) (this Unique) { // 工厂函数
 	switch typ {
 	default:
+	// TODO: case "sql_mem":
 	case "file_mem":
 		data, _ := json.Marshal(args)
-		this = &UnrepeatableFileMem{}
+		this = &UniqueFileMem{}
 		json.Unmarshal(data, this)
 		break
 	}
 	return
 }
 
-type UnrepeatableFileMem struct { // 使用内存HashSet来保证唯一性(非进程安全)
+type UniqueFileMem struct { // 使用内存HashSet来保证唯一性(非进程安全)
 	Unique
 
-	CacheContainer       map[string]bool
-	PersistenceContainer string `json:"stonefile"`
+	containerCache       map[string]bool // 缓存容器
+	containerPersistence LineWriter      `json:"stonefile"` // 持久容器
 }
 
-func (this *UnrepeatableFileMem) Put(id string) bool {
+func (this *UniqueFileMem) Put(id string) (err error) {
 	if this.CanPut(id) {
-		_, err := io.WriteString(this.PersistenceContainer, id+"\n")
+		err = this.containerPersistence.WriteLine(id)
 		if err != nil {
 			Log("Fail to Write File:", err)
-			return false
+			return
 		}
-		this.CacheContainer[id] = true
+		this.containerCache[id] = true
 	}
-	return false
+	return
 }
-func (this *UnrepeatableFileMem) CanPut(id string) bool {
-	if this.CacheContainer[id] {
+func (this *UniqueFileMem) CanPut(id string) bool {
+	if this.containerCache[id] {
 		return false
 	}
 	return true

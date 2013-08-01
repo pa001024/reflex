1dbb24ba8372ce33e6348159f6666863f344fdef
pa001024 <pa001024@qq.com>
2013-08-01 15:00:22 +0800

    update color render

--- util/ascgen/ShowImage.go
+++ util/ascgen/ShowImage.go
@@ -61,68 +61,134 @@ func ShowPng(iw io.Writer, r io.Reader, console Console, useColor bool) (err err
 	return
 }
 
+const clist64 = "MMMNNBBEEFRRWW9956OGQDKU33ICJXV7i11jjllllrr;;;;:::,,,,.....     "
+
 // 将图片渲染到文字(有颜色版)
 func ShowColor(iw io.Writer, img image.Image, console Console) {
-	const clist = "MMMMMMMMMM@@@@@@@@@$$$$$$$$$WWWWWWWWW#######BBBBBBBB000000000888888888mmmmmmmmmmmmHHHHHHHHHHHHZZZZZZZZZZEEEEEEEEaaaaaaa2222222SSSSSSSXXXXXXXXXXXnnnnnnnn777777777jjjjjjjlllllllvvvvvvvvzzzzzzzzrrrrrrrrriiiiiiiii;;;;;;;;::::::::::,,,,,,,...........           " //"       ......,,,,,,,,-------========:::::::;;;;;;;/////////////aaaaaaaammmmmmmbbbbbbbbkkkkkkkkDDDDDEEEHHHHRRRRQQQQQQQ$$$$$$%%%%%%%%"
 	// Lanczos3缩放图片到控制台屏幕大小
 	w := console.ScreenW - 1 // 标宽 换行符竟然也算一个?
 	s := img.Bounds().Size()
 	// 标高 = 结果高 * 宽高比 = (原高 / 原宽 * 标宽) * 宽高比
 	h := uint(float64(s.Y) / float64(s.X) * float64(w) * console.Ratio())
-	m := resize.Resize(w, h, img, resize.Lanczos3)
+	println("loaded")
+	m := resize.Resize(w, h, img, resize.Bicubic)
+	println("resized")
 	for y := 0; y < int(h); y++ {
 		for x := (0); x < int(w); x++ {
 			r, g, b, a := m.At(x, y).RGBA()
-			ct.ChangeColor(GetConsoleColor(r, g, b, a))
-			cindex := (r + g + b) / 3 * a / 65536 / 256
-			io.WriteString(iw, clist[cindex:cindex+1])
+			r, g, b, a = r>>10, g>>10, b>>10, a>>10 // 转换到6位色 0~63
+			r, g, b = r*a/63, g*a/63, b*a/63        // Alpha混合
+			m, fg, fgl, bg, bgl := GetConsoleColor(r, g, b)
+			ct.ChangeColor(fg, fgl, bg, bgl)
+			// cindex := (r + g + b) / 3
+			io.WriteString(iw, clist64[m:m+1])
 		}
 		io.WriteString(iw, "\n")
 	}
 	ct.ResetColor()
 }
 
-func GetConsoleColor(r, g, b, a uint32) (fg ct.Color, fgl bool, bg ct.Color, bgl bool) {
-	r, g, b = (r*a/65536/256+32)/64, (g*a/65536/256+32)/64, (b*a/65536/256+32)/64 // 从16位颜色转换到3位颜色 Alpha混合底色为黑
-	type truecolor struct{ r, g, b uint32 }
-	type fbcolor struct {
-		c ct.Color
-		b bool
+var (
+	rgb1_c = map[rgb1]ct.Color{
+		rgb1{0, 0, 0}: ct.Black,
+		rgb1{1, 1, 1}: ct.White,
+		rgb1{1, 0, 0}: ct.Red,
+		rgb1{0, 1, 0}: ct.Green,
+		rgb1{0, 0, 1}: ct.Blue,
+		rgb1{1, 1, 0}: ct.Yellow,
+		rgb1{0, 1, 1}: ct.Cyan,
+		rgb1{1, 0, 1}: ct.Magenta,
 	}
-	c2i := map[truecolor]fbcolor{
-		truecolor{0, 0, 0}: {ct.Black, false},
-		truecolor{1, 0, 0}: {ct.Red, false},
-		truecolor{0, 1, 0}: {ct.Green, false},
-		truecolor{0, 0, 1}: {ct.Blue, false},
-		truecolor{1, 1, 0}: {ct.Yellow, false},
-		truecolor{0, 1, 1}: {ct.Cyan, false},
-		truecolor{1, 0, 1}: {ct.Magenta, false},
-		truecolor{1, 1, 1}: {ct.White, false},
+)
+
+type rgb1 struct{ r, g, b uint32 }
+type rgb2 struct{ r, g, b uint32 }
+type rgb6 struct{ r, g, b uint32 }
+type rgb1b struct {
+	r, g, b uint32
+	l       bool
+}
+
+func (r rgb1b) ccolor() (ct.Color, bool) { return rgb1_c[rgb1{r.r, r.g, r.b}], r.l }
+func (r rgb1b) rgb6() rgb6 {
+	d := uint32(31)
+	if r.l {
+		d = 63
 	}
-	sr, sg, sb := (r+1)/2, (g+1)/2, (b+1)/2                        //去掉1位
-	bl := sr > 1 && sg > 1 || sr > 1 && sb > 1 || sg > 1 && sb > 1 // 计算亮度
-	sr, sg, sb = (r+2)/4, (g+2)/4, (b+2)/4                         //去掉2位
-	bgi := c2i[truecolor{sr, sg, sb}]
-	bg, bgl = bgi.c, bl
-	fg, fgl = ct.White, true
+	return rgb6{r.r * d, r.g * d, r.b * d}
+}
+func (r rgb2) rgb1b() rgb1b {
+	return rgb1b{(r.r + 1) / 2, (r.g + 1) / 2, (r.b + 1) / 2, (r.r+r.g+r.b) > 3 || r.r > 1 || r.g > 1 || r.b > 1}
+}
+func (r rgb6) rgb2() rgb2          { return rgb2{(r.r + 16) / 32, (r.g + 16) / 32, (r.b + 16) / 32} }
+func (r rgb6) sub(d rgb6) rgb6     { return rgb6{r.r - d.r, r.g - d.g, r.b - d.b} }
+func (r rgb6) times(t uint32) rgb6 { return rgb6{r.r * t, r.g * t, r.b * t} }
+
+func GetConsoleColor(r, g, b uint32) (m int, fg ct.Color, fgl bool, bg ct.Color, bgl bool) {
+	// 从6位颜色转换到2位颜色
+	h := rgb6{r, g, b}
+	// 背景色使用最接近的颜色
+	j := h.rgb2().rgb1b()
+	bg, bgl = j.ccolor()
+	// 仿色:
+	// 统计最高混合比: 字符'M' 50% 最低 字符' ' 0%
+	// 理论可用率66% 最高精确度 6位色
+	// 设: 实色h = rgb6(12,56,31) = rgb2(0,2,1) = rgb1b(0,1,0,false) = rgb6(0,31,0)
+	//     补色f rgb6(x,y,z)
+	//     补色混合比 = (m/n)
+	// 则: h = (f * (m/n) + h.rgb1b/2
+	//   (rgb6(x,y,z)*n + rgb1b(0,1,0,false))/2 = rgb6(12,56,31)
+	//   rgb6(x,y,z)*n = rgb6(12,56,31)*2 - rgb6(0,31,0)
+	//   rgb6(x,y,z)*(m/63) = rgb6(24,81,62)
+	//   rgb2(1,2,2)*(m/63) = rgb1b(0,1,1,true)*(m/63) = rgb6(0,63,63)/63*m
+	//   {
+	//      rgb6(x,y,z) = rgb6(0,63,63) = h*2-h.rgb1b
+	//      m = (56+31)/2=44 = (Z[h](h.?>1)/C[h](h.?>1))
+	//   }
+	f := h.times(2).sub(j.rgb6())
+	fc := f.rgb2().rgb1b()
+	m, n := 0, 0
+	if fc.r > 0 {
+		m += int(f.r)
+		n++
+	}
+	if fc.g > 0 {
+		m += int(f.g)
+		n++
+	}
+	if fc.b > 0 {
+		m += int(f.b)
+		n++
+	}
+	if n > 0 {
+		m = 63 - (m / n)
+	}
+	fg, fgl = fc.ccolor()
 	return
+}
+
+// 实用小函数 模拟 b?l:r
+func sw(b bool, l, r uint32) uint32 {
+	if b {
+		return l
+	}
+	return r
 }
 
 // 将图片渲染到文字
 func Show(iw io.Writer, img image.Image, console Console) {
-	const clist = "MMMMMMMMMM@@@@@@@@@$$$$$$$$$WWWWWWWWW#######BBBBBBBB000000000888888888mmmmmmmmmmmmHHHHHHHHHHHHZZZZZZZZZZEEEEEEEEaaaaaaa2222222SSSSSSSXXXXXXXXXXXnnnnnnnn777777777jjjjjjjlllllllvvvvvvvvzzzzzzzzrrrrrrrrriiiiiiiii;;;;;;;;::::::::::,,,,,,,...........           " //"       ......,,,,,,,,-------========:::::::;;;;;;;/////////////aaaaaaaammmmmmmbbbbbbbbkkkkkkkkDDDDDEEEHHHHRRRRQQQQQQQ$$$$$$%%%%%%%%"
-	// Lanczos3缩放图片到控制台屏幕大小
+	// Bicubic缩放图片到控制台屏幕大小
 	w := console.ScreenW - 1 // 标宽 换行符竟然也算一个?
 	s := img.Bounds().Size()
 	// 标高 = 结果高 * 宽高比 = (原高 / 原宽 * 标宽) * 宽高比
 	h := uint(float64(s.Y) / float64(s.X) * float64(w) * console.Ratio())
-	m := resize.Resize(w, h, img, resize.Lanczos3)
+	m := resize.Resize(w, h, img, resize.Bicubic)
 	bw := bufio.NewWriter(iw)
 	for y := 0; y < int(h); y++ {
 		for x := (0); x < int(w); x++ {
 			r, g, b, a := m.At(x, y).RGBA()
-			cindex := (r + g + b) / 3 * a / 65536 / 256
-			io.WriteString(bw, clist[cindex:cindex+1])
+			cindex := (r + g + b) / 3 * a / 65536 / 256 / 4
+			io.WriteString(bw, clist64[cindex:cindex+1])
 		}
 		io.WriteString(bw, "\n")
 	}
--- util/ascgen/ShowImage_test.go
+++ util/ascgen/ShowImage_test.go
@@ -14,39 +14,40 @@ func TestShow(t *testing.T) {
 		t.Fail()
 		return
 	}
-	err = ShowJpg(os.Stdout, f, Console{6, 14, 120})
+	err = ShowJpg(os.Stdout, f, Console{6, 14, 120}, false)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
 func TestShowColor(t *testing.T) {
-	f, err := os.Open("lol.png")
+	f, err := os.Open("cc.png")
 	if err != nil {
-		t.Fail()
-		return
+		t.Fatal(err)
 	}
-	err = ShowPngColor(os.Stdout, f, Console{6, 14, 120})
+	err = ShowPng(os.Stdout, f, Console{6, 14, 120}, true)
 	if err != nil {
-		fmt.Println(err)
+		t.Fatal(err)
 	}
 }
 
 func TestGetConsoleColor(t *testing.T) {
-	for b := uint32(0); b <= 5; b++ {
-		for g := uint32(0); g <= 5; g++ {
-			for r := uint32(0); r <= 5; r++ {
-				// fmt.Println("RGB:", r*0x33, g*0x33, b*0x33)
-				g, h, j, k := GetConsoleColor(r*256*0x33, g*256*0x33, b*256*0x33, 65536)
-				ct.ChangeColor(g, h, j, k)
+	for b := uint32(0); b < 6; b++ {
+		for g := uint32(0); g < 6; g++ {
+			for r := uint32(0); r < 6; r++ {
+				m, l, h, j, k := GetConsoleColor(r*0x33/4, g*0x33/4, b*0x33/4)
+				ct.ChangeColor(l, h, j, k)
+				c := clist64[m : m+1]
+				c = c + c + c + c
 				if k {
-					fmt.Print(j << 1)
+					fmt.Printf("%s%x", c, (j-1)<<1)
 				} else {
-					fmt.Print(j)
+					fmt.Printf("%s%x", c, (j - 1))
 				}
-				fmt.Print(" ")
 				ct.ResetColor()
+				fmt.Printf("#%02x%02x%02x ", r*0x33, g*0x33, b*0x33)
 			}
+			fmt.Print("\n")
 		}
 	}
 }

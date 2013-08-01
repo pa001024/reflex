b211b9f0ff8b306ac9c887fffcc198ba01edf7d3
pa001024 <pa001024@qq.com>
2013-08-01 15:53:53 +0800

    add ShowSimpleColor and update cli

--- util/ascgen/ShowImage.go
+++ util/ascgen/ShowImage.go
@@ -3,9 +3,6 @@ package image
 import (
 	"bufio"
 	"image"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
 	"io"
 
 	ct "github.com/daviddengcn/go-colortext"
@@ -22,43 +19,6 @@ func (c Console) Width() uint {
 }
 func (c Console) Ratio() float64 {
 	return float64(c.FontW) / float64(c.FontH)
-}
-
-func ShowGif(iw io.Writer, r io.Reader, console Console, useColor bool) (err error) {
-	img, err := gif.Decode(r)
-	if err != nil {
-		return
-	}
-	if useColor {
-		ShowColor(iw, img, console)
-	} else {
-		Show(iw, img, console)
-	}
-	return
-}
-func ShowJpg(iw io.Writer, r io.Reader, console Console, useColor bool) (err error) {
-	img, err := jpeg.Decode(r)
-	if err != nil {
-		return
-	}
-	if useColor {
-		ShowColor(iw, img, console)
-	} else {
-		Show(iw, img, console)
-	}
-	return
-}
-func ShowPng(iw io.Writer, r io.Reader, console Console, useColor bool) (err error) {
-	img, err := png.Decode(r)
-	if err != nil {
-		return
-	}
-	if useColor {
-		ShowColor(iw, img, console)
-	} else {
-		Show(iw, img, console)
-	}
-	return
 }
 
 const clist64 = "MMMNNBBEEFRRWW9956OGQDKU33ICJXV7i11jjllllrr;;;;:::,,,,.....     "
@@ -180,6 +140,29 @@ func sw(b bool, l, r uint32) uint32 {
 	return r
 }
 
+// 将图片渲染到彩色文字
+func ShowSimpleColor(iw io.Writer, img image.Image, console Console) {
+	// Bicubic缩放图片到控制台屏幕大小
+	w := console.ScreenW - 1 // 标宽 换行符竟然也算一个?
+	s := img.Bounds().Size()
+	// 标高 = 结果高 * 宽高比 = (原高 / 原宽 * 标宽) * 宽高比
+	h := uint(float64(s.Y) / float64(s.X) * float64(w) * console.Ratio())
+	m := resize.Resize(w, h, img, resize.Bicubic)
+	for y := 0; y < int(h); y++ {
+		for x := (0); x < int(w); x++ {
+			r, g, b, a := m.At(x, y).RGBA()
+			r, g, b, a = r>>10, g>>10, b>>10, a>>10 // 转换到6位色 0~63
+			r, g, b = r*a/63, g*a/63, b*a/63        // Alpha混合
+			_, _, _, bg, bgl := GetConsoleColor(r, g, b)
+			ct.ChangeColor(bg, bgl, ct.None, false)
+			cindex := (r + g + b) / 3
+			io.WriteString(iw, clist64[cindex:cindex+1])
+		}
+		io.WriteString(iw, "\n")
+	}
+	ct.ResetColor()
+}
+
 // 将图片渲染到文字
 func Show(iw io.Writer, img image.Image, console Console) {
 	// Bicubic缩放图片到控制台屏幕大小
@@ -192,7 +175,7 @@ func Show(iw io.Writer, img image.Image, console Console) {
 	for y := 0; y < int(h); y++ {
 		for x := (0); x < int(w); x++ {
 			r, g, b, a := m.At(x, y).RGBA()
-			cindex := (r + g + b) / 3 * a / 65536 / 256 / 4
+			cindex := (r + g + b) / 3 * a / 65535 / 256 / 4
 			io.WriteString(bw, clist64[cindex:cindex+1])
 		}
 		io.WriteString(bw, "\n")
--- util/ascgen/ascgen/ascgen-cli.go
+++ util/ascgen/ascgen/ascgen-cli.go
@@ -3,18 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
 	"os"
-	"path"
 
 	asc "github.com/pa001024/MoeWorker/util/ascgen"
 )
 
+func Show(iw io.Writer, r io.Reader, console asc.Console, useColor, simple bool) (err error) {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return
+	}
+	switch {
+	case useColor && simple:
+		asc.ShowSimpleColor(iw, img, console)
+	case useColor && !simple:
+		asc.ShowColor(iw, img, console)
+	default:
+		asc.Show(iw, img, console)
+	}
+	return
+}
+
 func main() {
-	pF := flag.String("f", "", "the file you want to render")
+	pF := flag.String("i", "", "the file you want to render")
 	pW := flag.Uint("w", 120, "Console.width")
 	pN := flag.Uint("n", 6, "Font.width")
 	pH := flag.Uint("h", 14, "Font.height")
 	pC := flag.Bool("c", false, "Show Color")
+	pS := flag.Bool("s", false, "Show Color Simple")
 	flag.Parse()
 
 	if *pF == "" {
@@ -22,20 +43,12 @@ func main() {
 		return
 	}
 
-	ext := path.Ext(*pF)
 	console := asc.Console{*pN, *pH, *pW}
 	r, err := os.Open(*pF)
 	if err != nil {
 		fmt.Println(err)
 	}
-	switch ext {
-	default:
-		err = asc.ShowPng(os.Stdout, r, console, *pC)
-	case ".jpg", ".jpeg":
-		err = asc.ShowJpg(os.Stdout, r, console, *pC)
-	case ".gif":
-		err = asc.ShowGif(os.Stdout, r, console, *pC)
-	}
+	Show(os.Stdout, r, console, *pC, *pS)
 	if err != nil {
 		fmt.Println(err)
 	}
